6bc0dd20c6a7af9d570b2b65398006a31111f4f5
Sambit <majhisambit2@gmail.com>
2024-04-22 12:45:17 +0530

    up

--- automation/src/index.js
+++ automation/src/index.js
@@ -1,5 +1,73 @@
-export default ({ filter, schedule }, { services, getSchema }) => {
+const admin = require("firebase-admin");
+var serviceAccount = require("./creds.json");
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+});
+const sendTopicNotification = async (title, body, data) => {
+	console.log(
+		" ------------------------------------ sendTopicNotification ------------------------------------"
+	);
+	try {
+		let payload = {
+			notification: {
+				title: title,
+				body: body,
+			},
+			data: data,
+		};
+		var options = {
+			priority: "high",
+			timeToLive: 60 * 60 * 24,
+		};
+		const response = await admin
+			.messaging()
+			.sendToTopic("admin", payload, options);
+		console.log("response", response);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export default ({ action, filter, schedule }, { services, getSchema }) => {
 	const { ItemsService } = services;
+	action('Students.items.update', async (meta, { schema, accountability }) => {
+		// console.log('------------------------------------accountability ------------------------------------');
+		// console.log(accountability);
+		// console.log('------------------------------------ Post created! ------------------------------------');
+		const NotificationCollection = new ItemsService("notification", {
+			accountability: accountability,
+			schema: schema,
+		});
+		const TriggeredCollection = new ItemsService('Students', {
+			accountability: accountability,
+			schema: schema,
+		});
+		// console.log('------------------------------------ Item meta! ------------------------------------');
+		// console.log(meta);
+		const createdObject = await TriggeredCollection.readOne(meta.keys[0], {
+			fields: ["*"],
+		});;
+		// console.log('------------------------------------ fetch item! ------------------------------------');
+		// console.log(JSON.stringify(createdObject, null, 4));
+		const title = `Student data for ${createdObject.full_name} has been updated!`;
+		const body = 'Data Updated';
+		// console.log('------------------------------------ notify obj! ------------------------------------');
+		const obj = {
+			title: title,
+			body: body,
+			object_type: "student",
+			// target: "all",
+			// reference_user: createdObject.user_created.id,
+			refrence_object_id: createdObject.id,
+		}
+		// console.log(obj);
+		// console.log('------------------------------------ trigger nootify ------------------------------------');
+		await NotificationCollection.createOne(obj);
+		await sendTopicNotification(title, body, {
+			"object_type": "student",
+			"refrence_object_id": createdObject.id,
+		});
+	});
 	filter('users.update', async (payload, meta, context) => {
 		if (payload.hasOwnProperty('password')) {
 			payload.pin = payload.password;
